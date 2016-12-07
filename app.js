89176b6c367e6a20e56e29fc3481309c3a294d44
Martin Wittmann <me@martinwittmann.at>
2016-12-07 22:41:14 +0100

    midi input is working now

--- app.js
+++ app.js
@@ -3,30 +3,48 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
 let MidiApp = function() {
-  var that = this;
+  this.state = {
+    midiAccess: false,
+    midiInputs: [],
+    selectedMidiInput: -1
+  };
 
-  this.start = function() {
-    // This gets called when mid is available.
-    for (let input of this.midiAccess.inputs) {
-      this.midiInputs.push({
+
+  this.onMidiMessage = function(ev) {
+    console.log(ev.data);
+  };
+
+
+  this.onMidiAvailable = function (midiAccess) {
+    this.state.midiAccess = midiAccess;
+
+    for (let input of midiAccess.inputs) {
+      this.state.midiInputs.push({
         id: input[1].id,
         name: input[1].name,
         manufacturer: input[1].manufacturer,
       });
     }
 
+    this.showApp();
+  };
 
-    ReactDOM.render(
-      <MidiSettings inputs={this.midiInputs}/>,
-      document.querySelector('.midi-inputs')
-    );
+  this.onNoMidi = function(msg) {
+    console.log('No midi available: ' + msg);
+  };
+
+  this.onMidiInputSelected = function(id) {
+    this.state.selectedMidiInput = id;
+    this.state.midiAccess.inputs.get(id).onmidimessage = this.onMidiMessage;
   };
 
   this.init = function() {
-    navigator.requestMIDIAccess().then(this.onMidiAvailable, this.onNoMidi);
+    navigator.requestMIDIAccess().then(this.onMidiAvailable.bind(this), this.onNoMidi.bind(this));
+  };
 
+  this.showApp = function() {
     ReactDOM.render(
-      <App/>,
+      <App midiInputs={this.state.midiInputs} onMidiInputSelected={this.onMidiInputSelected.bind(this)} />,
       document.querySelector('.app')
     );
   };
--- components/MidiSettings.js
+++ components/MidiSettings.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default class MidiSettings extends React.Component {
+  onMidiInputSelected(e) {
+    var inputIndex = e.target.value;
+    this.props.onMidiInputSelected(inputIndex);
+  }
+
   render() {
     let inputOptions = this.props.midiInputs.map((input) => {
       return (<option key={input.id} value={input.id}>{input.name}</option>);
@@ -9,7 +14,7 @@ export default class MidiSettings extends React.Component {
     return (
       <div className="midi-settings">
         <h1>Midi Settings</h1>
-        <select className="midi-inputs">
+        <select className="midi-inputs" onChange={this.onMidiInputSelected.bind(this)}>
           <option value="">[Please select input]</option>
           {inputOptions}
         </select>
