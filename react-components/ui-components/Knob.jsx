294179f4b4c42c21db85d5a5e27f90f75cb963a1
Martin Wittmann <me@martinwittmann.at>
2016-12-26 12:28:42 +0100

    working on knob ux

--- react-components/ui-components/Knob.jsx
+++ react-components/ui-components/Knob.jsx
@@ -7,16 +7,26 @@ export default class Knob extends React.Component {
       value : props.defaultValue
     };
 
-    this.minValue = props.min || 0;
-    this.maxValue = props.max || 1;
     this.knobSize = 40;
     this.strokeWidth = this.knobSize / 15;
-    this.buttonColor = '#ccc';
+    this.buttonColor = '#aaaaaa';
     this.rangeAngle = 280; // The angle between the min and the max points of the knob.
+    this.padding = {
+      top: 10,
+      right: 10,
+      bottom: 30,
+      left: 10
+    };
+    this.fullWidth = this.padding.left + this.knobSize + this.padding.right;
+    this.fullHeight = this.padding.top + this.knobSize + this.padding.bottom;
 
-    this.mouseScaleFactor = .5;
+    this.minValue = props.min || 0;
+    this.maxValue = props.max || 1;
+    this.mouseScaleFactor = .005;
 
+    // Bind all event handlers which will be added to window to this.
     this.onMouseMove = this.onMouseMove.bind(this);
+    this.onWindowMouseUp = this.onWindowMouseUp.bind(this);
   }
 
   onChange(ev) {
@@ -33,69 +43,97 @@ export default class Knob extends React.Component {
     let value = Math.max(this.minValue, Math.min(this.state.value, this.maxValue));
     let valueRange = this.maxValue - this.minValue;
     let factor = this.rangeAngle / valueRange;
-    return value * factor - this.rangeAngle / 2;
+    return (value * factor - this.rangeAngle / 2) * 1;
   }
 
   getKnobTransform() {
     let angle = this.getAngle();
-    let offset = this.knobSize / 2 + this.strokeWidth;
-    return 'rotate(' + angle + ' ' + offset + ' ' + offset + ')';
+    let offsetX = this.knobSize / 2 + this.strokeWidth + this.padding.left;
+    let offsetY = this.knobSize / 2 + this.strokeWidth + this.padding.top;
+    return 'rotate(' + angle + ' ' + offsetX + ' ' + offsetY + ')';
   }
 
   onMouseDown(ev) {
-    this.containerDomNode.addEventListener('mousemove', this.onMouseMove, true);
-    this.mousePosX = ev.pageX;
+    window.addEventListener('mousemove', this.onMouseMove);
+    window.addEventListener('mouseup', this.onWindowMouseUp);
+    this.mousePosY = ev.pageY;
+    this.valueWasChanged = true;
 
+    // Prevent moving the knob from dragging the component around.
     ev.preventDefault();
-    ev.stopPropagation();
-    return false;
   }
 
-  onMouseUp(ev) {
-    console.log('removeEventListener');
-    this.containerDomNode.removeEventListener('mousemove', this.onMouseMove);
+  onWindowMouseUp(ev) {
+    window.removeEventListener('mousemove', this.onMouseMove);
+    window.removeEventListener('mouseup', this.onWindowMouseUp);
+    this.valueWasChanged = false;
+  }
+
+  onKnobMouseUp(ev) {
+    console.log(ev.type, this.valueWasChanged);
+    // Prevent selecting the component everytime the knob is moved.
+    if (this.valueWasChanged) {
+      this.valueWasChanged = false;
+      ev.stopPropagation();
+      ev.preventDefault();
+    }
   }
 
   onMouseMove(ev) {
-    let xOffset = ev.pageX - this.mousePosX;
-    console.log(xOffset);
-    this.setState({
-      value: this.state.value + xOffset * this.mouseScaleFactor
-    });
+    let rawValue = (ev.pageY - this.mousePosY) * -1;
+    let newValue = this.state.value + rawValue * this.mouseScaleFactor;
+
+    this.mousePosY = ev.pageY;
+    this.setValue(newValue);
+  }
+
+  setValue(value) {
+    value = Math.max(this.minValue, Math.min(value, this.maxValue));
+
+    if (value != this.state.value) {
+      this.setState({
+        value
+      });
+    }
   }
 
   render() {
+    let centerX = (this.knobSize) / 2 + this.strokeWidth + this.padding.left;
+    let centerY = (this.knobSize) / 2 + this.strokeWidth + this.padding.top;
+    let knobRadius = this.knobSize / 2;
+
     return (
       <div
         className="input-knob"
-        ref={el => {
-          this.containerDomNode = el;
-        }}
+        onMouseUp={this.onKnobMouseUp.bind(this)}
       >
         <input type="hidden" value={this.state.value} />
-        <svg className="input-svg">
+        <svg
+          className="input-svg"
+          width={this.fullWidth}
+          height={this.fullHeight}
+        >
           <g
             className="knob"
             transform={this.getKnobTransform()}
             onMouseDown={this.onMouseDown.bind(this)}
-            onMouseUp={this.onMouseUp.bind(this)}
             ref={el => {
               this.domNode = el;
             }}
           >
             <circle
-              cx={this.knobSize / 2 + this.strokeWidth}
-              cy={this.knobSize / 2 + this.strokeWidth}
-              r={this.knobSize / 2}
+              cx={centerX}
+              cy={centerY}
+              r={knobRadius}
               fill="transparent"
               stroke={this.buttonColor}
               strokeWidth={this.strokeWidth}
             />
             <line
-              x1={this.knobSize / 2 + this.strokeWidth}
-              y1={this.strokeWidth}
-              x2={this.knobSize / 2 + this.strokeWidth}
-              y2={this.knobSize / 2 + this.strokeWidth}
+              x1={centerX}
+              y1={this.strokeWidth + this.padding.top}
+              x2={centerX}
+              y2={centerY}
               stroke={this.buttonColor}
               strokeWidth={this.strokeWidth}
             />
